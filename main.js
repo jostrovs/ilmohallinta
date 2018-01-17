ff43253f0162be0b01acf65516c2a221475b8967
Jori Ostrovskij <jori.ostrovskij@granlund.fi>
2018-01-17 15:04:34 +0200

    Auth alkua

--- main.js
+++ main.js
@@ -10,6 +10,25 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
+// Auth
+var provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
 Vue.component('vue-hello', {
     template:` 
     <p id="hello">jesjes {{greet}}</p>                                                                                         
@@ -33,12 +52,23 @@ new Vue({
         ilmoittautuneet: [],
         title: "Ilmohallinta",
         loading: 2,
+        editor: false,
+        next_id: 0,
+
+        add_nimi: "",
+        add_sarja: "",
     },
     beforeCreate(){
         let self = this;
         self.loading++;
         database.ref('/ilmoittautuneet').once('value').then(function(joukkueet) {
             joukkueet.forEach(function(joukkue){
+                let key = parseInt(joukkue.key, 10);
+                if(key >= self.next_id){
+                     self.next_id = key+1;
+                     console.log("Next id asetettu " + self.next_id);
+                }
+                
                 let val = joukkue.val();
                 console.log("Lisätään joukkue " + val.nimi + " sarjaan " + val.sarja);
                 self.ilmoittautuneet.push(val);
@@ -67,6 +97,21 @@ new Vue({
                 });
             }
             return ret;
+        },
+
+        add_disabled(){
+            return this.add_nimi.length < 1 || this.add_sarja.length < 1;
+        }
+    },
+
+    methods: {
+        adder(){
+            firebase.database().ref('ilmoittautuneet/' + this.next_id++).set({
+                nimi: this.add_nimi,
+                sarja: this.add_sarja,
+            });           
+            this.editor = false;
+            location.reload();
         }
     }
 })
